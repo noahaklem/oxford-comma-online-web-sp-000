448129651385a3360aa6eda99ac428a36459b2b6
Noah  Klem <klem.noah@gmail.com>
2021-05-27 19:37:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join()
+    elsif array.count == 2
+      array.join(" and ")
+      elsif array.count == 3
+        array.insert(2, "and")
+        array.join(", ")
+  end
+  
 end
