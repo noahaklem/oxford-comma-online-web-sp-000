95d0823728a7fd5faed0c81679804904259508ea
Noah  Klem <klem.noah@gmail.com>
2021-05-27 19:53:18 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    array.join()
+  
+  when 2
+    array.join(" and ")
+  
+  else
+    last_item = array.last
+    delete_last = array.delete(last_item)
+    array = array.join(', ') << ", and #{last_item}"
+  end
+  
 end
